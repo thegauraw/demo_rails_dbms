8226f6df92b3a73e2cd2e93915a1debe582d0a53
thegauraw <thegauraw@gmail.com>
2016-01-02 23:37:40 +0545

    Players part functional with nested-routes.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,57 @@
 class PlayersController < ApplicationController
+  before_filter :authenticate, :except => [:index, :show]
+
   def index
+    @club = Club.find(params[:club_id])
   end
+
+  def show
+    @player = Player.find(params[:id])
+  end
+
+  def new
+    @sport = Sport.find(params[:sport_id])
+    @club = Club.find(params[:club_id])
+    @player = Player.new(club: @club)
+  end
+
+  def create
+    @sport = Sport.find(params[:sport_id])
+    @club = Club.find(params[:club_id])
+    @player = @club.players.new(player_params)
+    if @player.save
+      redirect_to sport_club_player_path(@sport.id, @club.id, @player)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @player = Player.find(params[:id])
+    @club = @player.club
+    @sport = @club.sport
+  end
+
+  def update
+    @player = Player.find(params[:id])
+    @club = @player.club
+    @sport = @club.sport
+    if @player.update(player_params)
+      redirect_to sport_club_player_path(@sport.id, @club.id, @player)
+    else
+      render 'edit'
+    end
+  end
+
+  def search
+    @query = params[:query]
+    @results = Player.where("name LIKE :query", query: "%#{@query}%")
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name, :image_link, :country, :position, :description)
+  end
+
 end
